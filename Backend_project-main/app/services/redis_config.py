83380eefabad4896555b41a964185ffeb92fca0e
Mohamedmuhsen <152475883+Mohamedmuhsen@users.noreply.github.com>
2026-05-26 09:55:03 +0200

    Delete Backend_project-main directory

--- Backend_project-main/app/services/redis_config.py
+++
@@ -1,9 +0,0 @@
-"""
-Redis client is configured centrally in app/database.py.
-Import redis_client from there:
-
-    from ..database import redis_client
-"""
-from .database import redis_client  # re-export for convenience
-
-__all__ = ["redis_client"]
